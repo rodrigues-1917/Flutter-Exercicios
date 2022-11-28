2d47f77f8a8bcd26480851437e24ed9535969f95
kalebe rodrigues <kaleberodrigues52@gmail.com>
2022-11-28 20:58:06 -0300

    Alteracao de cores

--- Layout/app_layout/lib/layout.dart
+++ Layout/app_layout/lib/layout.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // Para a classe Layout se comportar como widget dentro do flutter
 // extendemos a classe com um estado que pode ser alterado (stateful)
-class Layout extends StatefulWidget{
+class Layout extends StatefulWidget {
   // Deve se sobreescrever o metodo create state usando override
   @override
   // Criamos um estado da classe LayouState, onde esta nosso construtor build
@@ -16,9 +16,10 @@ class LayoutState extends State<Layout> {
     return MaterialApp(
       home: Scaffold(
         // Barra de titulo
-        appBar: AppBar(title: Text("Layout - Flutter")),
-
-
+        appBar: AppBar(
+          title: Text("Layout - Flutter"),
+          backgroundColor: Colors.green.shade700,
+        ),
 
         // Menu lateral (hamburguer)
         drawer: Drawer(
@@ -30,20 +31,20 @@ class LayoutState extends State<Layout> {
             children: <Widget>[
               // Cabecalho do menu
               DrawerHeader(
-                child: Text("Menu de ferramentas"),
-                decoration: BoxDecoration(color: Colors.blueAccent)
-              ),
+                  child: Text("Menu de ferramentas"),
+                  decoration: BoxDecoration(color: Colors.green.shade700)),
 
               // item 1 do menu
               ListTile(
                 title: Text("Flutter"),
                 subtitle: Text("flutterzinho"),
-                leading: Icon(Icons.flash_on,
+                leading: Icon(
+                  Icons.flash_on,
                   color: Colors.red.shade900,
                   size: 24,
-                  ),
+                ),
                 trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: (){
+                onTap: () {
                   Navigator.pop(context);
                 },
               ),
@@ -52,12 +53,10 @@ class LayoutState extends State<Layout> {
               ListTile(
                 title: Text("Dart"),
                 subtitle: Text("Dartzinho"),
-                leading: Icon(Icons.mood,
-                color: Colors.green.shade900,
-                size: 24
-                ),
+                leading:
+                    Icon(Icons.mood, color: Colors.green.shade900, size: 24),
                 trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: (){
+                onTap: () {
                   Navigator.pop(context);
                 },
               ),
@@ -66,43 +65,38 @@ class LayoutState extends State<Layout> {
               ListTile(
                 title: Text("VS"),
                 subtitle: Text("VSzinho"),
-                leading: Icon(Icons.rocket,
-                color: Colors.black,
-                size: 24
-                ),
+                leading: Icon(Icons.rocket, color: Colors.black, size: 24),
                 trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: (){
+                onTap: () {
                   Navigator.pop(context);
                 },
               ),
-            
+
               // item 3 do menu
               ListTile(
                 title: Text("Config"),
                 subtitle: Text("Configzinho"),
-                leading: Icon(Icons.settings,
-                color: Colors.teal,
-                size: 24
-                ),
+                leading: Icon(Icons.settings, color: Colors.teal, size: 24),
                 trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: (){
+                onTap: () {
                   Navigator.pop(context);
                 },
               ),
 
               // Versao do app
-              
             ],
           ),
         ),
 
         // Corpo do app
         body: Center(
-
           // Botao elevado
           child: ElevatedButton(
             // Titulo do botao
             child: Text("Mostrar SnackBar"),
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll<Color>(Colors.green.shade700)),
             // Quando for pressionado
             onPressed: () {
               // Final pois nao sera mais alterado
@@ -110,14 +104,14 @@ class LayoutState extends State<Layout> {
               // Aqui estamos criando ela (ainda nao estamos chamando)
               final snackBar = SnackBar(
                 // Texto que sera impresso na barra
-                content: Text("Buenas"),
+                content: Text("Os guri tao demais"),
                 // Duracao que ficara sendo exibido
-                duration: Duration(milliseconds: 2500),
+                duration: Duration(milliseconds: 40000),
                 // Comportamento da barra, nesse caso ira flutuar
                 behavior: SnackBarBehavior.floating,
                 // Acao da barra, sera o botao existente na barra
                 action: SnackBarAction(
-                  label: "Ok",
+                  label: "Com ctz",
                   onPressed: () {
                     // Codigo de alguma funcao
                   },
@@ -136,19 +130,15 @@ class LayoutState extends State<Layout> {
           items: const [
             // icone home
             BottomNavigationBarItem(
-              icon: Icon(Icons.water_drop),
-              label: "Home"),
+                icon: Icon(Icons.water_drop), label: "Home"),
 
             // icone configuracoes
             BottomNavigationBarItem(
-              icon:  Icon(Icons.thermostat_rounded),
-              label: "Configuracoes"
-              ),
+                icon: Icon(Icons.thermostat_rounded), label: "Configuracoes"),
 
             // icone login
             BottomNavigationBarItem(
-              icon: Icon(Icons.lightbulb_outline),
-              label: "Login")
+                icon: Icon(Icons.lightbulb_outline), label: "Login")
           ],
         ),
       ),
